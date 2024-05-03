d7e497cfaae17781f21be94b3f21977ce0c32db0
Danny:3 <megaspidey87@gmail.com>
2024-05-03 17:54:04 -0600

    Avance_Dashboard_SQL

--- BACKUP-SQL/PW2_Database_First.sql
+++ BACKUP-SQL/PW2_Database_First.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2024 a las 01:51:19
+-- Tiempo de generación: 01-05-2024 a las 01:11:17
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -20,132 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pw2`
 --
-
-DELIMITER $$
---
--- Procedimientos
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `publis_cat` (IN `usuario_id` INT)   BEGIN
-    -- Declara una variable para almacenar el ID de la categoría
-    DECLARE categoria_id INT;
-
-    -- Declara un cursor para obtener los IDs de categorías relacionadas al usuario
-    DECLARE cur CURSOR FOR
-        SELECT DISTINCT id_cat
-        FROM usu_cat
-        WHERE id_usuario = usuario_id;
-
-    -- Declara un manejador de errores para el cursor
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
-        SET categoria_id = NULL;
-
-    -- Abre el cursor
-    OPEN cur;
-
-    -- Inicializa la variable
-    SET categoria_id = NULL;
-
-    -- Recorre los resultados del cursor
-    read_loop: LOOP
-        FETCH cur INTO categoria_id;
-
-        -- Sal del bucle si no hay más resultados
-        IF categoria_id IS NULL THEN
-            LEAVE read_loop;
-        END IF;
-
-        -- Obtiene todas las publicaciones relacionadas a la categoría actual
-        SELECT DISTINCT p.id_publi, p.titulo_publi, p.desc_publi
-        FROM publicacion p
-        INNER JOIN publi_cat pc ON p.id_publi = pc.id_publi
-        WHERE pc.id_cat = categoria_id;
-
-    END LOOP;
-
-    -- Cierra el cursor
-    CLOSE cur;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `publis_newtoold` ()   BEGIN
-    -- Selecciona las publicaciones ordenadas del más nuevo al más viejo
-    SELECT id_publi, titulo_publi, desc_publi
-    FROM publicacion
-    ORDER BY fecha_publi DESC;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `publis_ufollowed` (IN `usuario_id` INT)   BEGIN
-    SELECT p.titulo_publi, p.foto_publi
-    FROM follow f
-    JOIN publicacion p ON f.id_ufollowed = p.id_autor
-    WHERE f.id_usuario = usuario_id;
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `publi_cat_p` (IN `id_publiT` INT, IN `nombre_cat` VARCHAR(255))   BEGIN
-
-    DECLARE categoria_idT INT;
- -- Obtener el ID de la categoría
-    SELECT id_cat INTO categoria_idT
-    FROM categoria
-    WHERE title_cat = nombre_categoria;
-
-    -- Insertar una fila en la tabla intermedia
-    INSERT INTO publi_cat (id_publi, id_cat)
-    VALUES (id_publiT, categoria_idT);
-END$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `usu_cat_p` (IN `usuario_id` INT, IN `publicacion_id` INT)   BEGIN
-    -- Declara una variable para almacenar el ID de la categoría
-    DECLARE categoria_id INT;
-
-    -- Declara una variable para almacenar temporalmente los resultados del cursor
-    DECLARE rows_affected INT;
-
-    -- Declara un cursor para almacenar los resultados de la consulta
-    DECLARE cur CURSOR FOR
-        SELECT id_cat
-        FROM publi_cat
-        WHERE id_publi = publicacion_id;
-
-    -- Declara un manejador de errores para el cursor
-    DECLARE CONTINUE HANDLER FOR NOT FOUND
-        SET categoria_id = NULL;
-
-    -- Abre el cursor
-    OPEN cur;
-
-    -- Inicializa la variable
-    SET categoria_id = NULL;
-
-    -- Inicializa la variable rows_affected
-    SET rows_affected = 0;
-
-    -- Recorre los resultados del cursor
-    read_loop: LOOP
-        FETCH cur INTO categoria_id;
-
-        -- Sal del bucle si no hay más resultados
-        IF categoria_id IS NULL THEN
-            LEAVE read_loop;
-        END IF;
-
-        -- Verificar si ya existe una entrada en la tabla intermedia para este par de IDs
-        SELECT COUNT(*)
-        INTO rows_affected
-        FROM usu_cat
-        WHERE id_usuario = usuario_id AND id_cat = categoria_id;
-
-        -- Si no existe una entrada, insertarla; de lo contrario, no hacer nada
-        IF rows_affected = 0 THEN
-            INSERT INTO usu_cat (id_usuario, id_cat)
-            VALUES (usuario_id, categoria_id);
-        END IF;
-    END LOOP;
-
-    -- Cierra el cursor
-    CLOSE cur;
-END$$
-
-DELIMITER ;
+CREATE DATABASE IF NOT EXISTS `pw2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pw2`;
 
 -- --------------------------------------------------------
 
@@ -153,6 +29,7 @@ DELIMITER ;
 -- Estructura de tabla para la tabla `categoria`
 --
 
+DROP TABLE IF EXISTS `categoria`;
 CREATE TABLE `categoria` (
   `id_cat` int(11) NOT NULL,
   `title_cat` varchar(55) DEFAULT NULL
@@ -181,6 +58,7 @@ INSERT INTO `categoria` (`id_cat`, `title_cat`) VALUES
 -- Estructura de tabla para la tabla `comentario`
 --
 
+DROP TABLE IF EXISTS `comentario`;
 CREATE TABLE `comentario` (
   `id_comm` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
@@ -194,11 +72,12 @@ CREATE TABLE `comentario` (
 -- Estructura de tabla para la tabla `follow`
 --
 
+DROP TABLE IF EXISTS `follow`;
 CREATE TABLE `follow` (
   `id_follow` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
   `id_ufollowed` int(11) DEFAULT NULL,
-  `fecha_follow` datetime DEFAULT NULL
+  `date_follow` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -207,6 +86,7 @@ CREATE TABLE `follow` (
 -- Estructura de tabla para la tabla `interaccion`
 --
 
+DROP TABLE IF EXISTS `interaccion`;
 CREATE TABLE `interaccion` (
   `id_inter` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
@@ -222,6 +102,7 @@ CREATE TABLE `interaccion` (
 -- Estructura de tabla para la tabla `likes`
 --
 
+DROP TABLE IF EXISTS `likes`;
 CREATE TABLE `likes` (
   `id_like` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
@@ -235,13 +116,13 @@ CREATE TABLE `likes` (
 -- Estructura de tabla para la tabla `publicacion`
 --
 
+DROP TABLE IF EXISTS `publicacion`;
 CREATE TABLE `publicacion` (
   `id_publi` int(11) NOT NULL,
   `id_autor` int(11) DEFAULT NULL,
   `titulo_publi` varchar(50) DEFAULT NULL,
   `desc_publi` varchar(255) DEFAULT NULL,
-  `fecha_publi` datetime DEFAULT NULL,
-  `foto_publi` mediumblob DEFAULT NULL
+  `email_usuario` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -250,6 +131,7 @@ CREATE TABLE `publicacion` (
 -- Estructura de tabla para la tabla `publi_cat`
 --
 
+DROP TABLE IF EXISTS `publi_cat`;
 CREATE TABLE `publi_cat` (
   `id_pcat` int(11) NOT NULL,
   `id_cat` int(11) DEFAULT NULL,
@@ -262,6 +144,7 @@ CREATE TABLE `publi_cat` (
 -- Estructura de tabla para la tabla `publi_comm`
 --
 
+DROP TABLE IF EXISTS `publi_comm`;
 CREATE TABLE `publi_comm` (
   `id_pcomm` int(11) NOT NULL,
   `id_comm` int(11) DEFAULT NULL,
@@ -274,6 +157,7 @@ CREATE TABLE `publi_comm` (
 -- Estructura de tabla para la tabla `publi_likes`
 --
 
+DROP TABLE IF EXISTS `publi_likes`;
 CREATE TABLE `publi_likes` (
   `id_pl` int(11) NOT NULL,
   `id_like` int(11) DEFAULT NULL,
@@ -286,9 +170,9 @@ CREATE TABLE `publi_likes` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
-  `foto_usuario` mediumblob NOT NULL,
   `nombre_usuario` varchar(50) DEFAULT NULL,
   `nickname_usuario` varchar(50) DEFAULT NULL,
   `desc_usuario` varchar(255) DEFAULT NULL,
@@ -296,19 +180,13 @@ CREATE TABLE `usuario` (
   `contrasenia_usuario` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id_usuario`, `foto_usuario`, `nombre_usuario`, `nickname_usuario`, `desc_usuario`, `email_usuario`, `contrasenia_usuario`) VALUES
-(1, '', 'moshiOnO', 'moshiOnO', 'Me gusta comer xD', 'moshiuwu@uanl.edu.mx', 'asdasdsad');
-
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `visualizacion`
 --
 
+DROP TABLE IF EXISTS `visualizacion`;
 CREATE TABLE `visualizacion` (
   `id_vis` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
@@ -463,7 +341,7 @@ ALTER TABLE `publi_likes`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `visualizacion`
